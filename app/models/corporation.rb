94fbf1ce8f5e1d669e2dd481ffa4778d06f0250c
Moshe Levi <mo6709@gmail.com>
2017-11-27 18:37:14 -0500

    update corporation model with has_many corporation_investments

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -3,4 +3,5 @@ class Corporation < ApplicationRecord
 	has_many :currencies, through: :currency_corporations
 	has_many :transactions
 	has_many :investors, through: :transactions
+	has_many :corporation_investments
 end
