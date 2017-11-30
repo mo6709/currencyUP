963e0ed7991d0346fc5b00944875aeca0c63432e
Moshe Levi <mo6709@gmail.com>
2017-11-30 18:12:13 -0500

    add assosiations to corporation model

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,7 +1,15 @@
 class Corporation < ApplicationRecord
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
+	# Include default devise modules.
+	devise :database_authenticatable, :registerable,
+	      :recoverable, :rememberable, :trackable, :validatable,
+	      :confirmable, :omniauthable
+	include DeviseTokenAuth::Concerns::User
+
+	serialize :regions_array, Array
+
+	has_many :currency_corporations
+	has_many :currencies, through: :currency_corporations
+	has_many :transactions
+	has_many :investors, through: :transactions
+	has_many :corporation_investments
 end
