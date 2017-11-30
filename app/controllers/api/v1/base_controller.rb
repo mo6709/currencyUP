2e9168451d2912a7e45c1e047cc518a2b50e0e6f
Moshe Levi <mo6709@gmail.com>
2017-11-30 14:30:38 -0500

    fix controller to work with devise_auth_token

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,3 +1,3 @@
-class Api::V1::BaseController < ActionController::API
-
+class Api::V1::BaseController < ApplicationController
+ 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+    before_action :configure_permitted_parameters
+
+    protected 
+       def configure_permitted_parameters 
+           devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :favorite_color])
+       end
 end
