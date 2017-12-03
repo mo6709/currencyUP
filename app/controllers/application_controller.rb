c0bf11ca200f6d96f5c70a0d0a71246561900244
Moshe Levi <mo6709@gmail.com>
2017-12-03 15:32:02 -0500

    fix application_controller to ommit email sign_up option for devise_token_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-    before_action :configure_permitted_parameters
+    before_action :devise_parameter_sanitizer
 
     protected 
-       def configure_permitted_parameters 
-           devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :favorite_color])
+       # def configure_permitted_parameters 
+       #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :title])
+       #     devise_parameter_sanitizer.permit(:update, keys: [])
+       # end
+       def devise_parameter_sanitizer
+           if resource_class == Investor
+               Investor::ParameterSanitizer.new(Investor, :investor, params)
+           elsif resource_class == Corporation
+               Corporation::ParameterSanitizer.new(Corporation, :corporation, params)
+           end
+           	
        end
 end
