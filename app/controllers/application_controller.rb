0d1b175d2b78f0ccce93653aa5689d51ea77f1b6
Moshe Levi <mo6709@gmail.com>
2017-12-04 18:53:39 -0500

    fixed application controller to run devise_parameter_sanitizer only for devise_contgroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-    before_action :devise_parameter_sanitizer
+    before_action :devise_parameter_sanitizer, if: :devise_controller?
 
-    protected 
-       # def configure_permitted_parameters 
-       #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :title])
-       #     devise_parameter_sanitizer.permit(:update, keys: [])
-       # end
-       def devise_parameter_sanitizer
-           if resource_class == Investor
-               Investor::ParameterSanitizer.new(Investor, :investor, params)
-           elsif resource_class == Corporation
-               Corporation::ParameterSanitizer.new(Corporation, :corporation, params)
-           end
-           	
+   def devise_parameter_sanitizer
+       if resource_class == Investor
+           Investor::ParameterSanitizer.new(Investor, :investor, params)
+       elsif resource_class == Corporation
+           Corporation::ParameterSanitizer.new(Corporation, :corporation, params)
        end
+       	
+   end
 end
