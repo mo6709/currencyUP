5d40c17a0d17e9d4337cf11a62508e0e79673571
Moshe Levi <mo6709@gmail.com>
2017-12-13 16:41:04 -0500

    add has_secure_password to corporation model. add serialize regions_arry to corporation

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,4 +1,7 @@
 class Corporation < ApplicationRecord
+	serialize :regions_array, Array
+	has_secure_password
+
 	has_many :currency_corporations
 	has_many :currencies, through: :currency_corporations
 	has_many :transactions
