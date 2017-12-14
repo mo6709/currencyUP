de71b128625b5befb36c3ef4924e79ebeaf16a9e
Moshe Levi <mo6709@gmail.com>
2017-12-14 10:50:07 -0500

    modified session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::SessionsController < Api::V1::BaseController
+	def investor_login
+		investor = Corporation.find_by(:email params[:email])
+
+		if investor && investor.authenticate(params[:password])
+			render(json: { token: Auth.create_token(investor) })
+		else
+			render(json: { errors, { 'Fale to login, oleas try again' } }, status: 500)
+		end
+	end
+
+	def corporation_login
+		corporation = Corporation.find_by(:email params[:email])
+
+		if corporation && corporation.authenticate(params[:password])
+			render(json: { token: Auth.create_token(corporation) })
+		else
+			render(json: { errors, { 'Fale to login, oleas try again' } }, status: 500)
+		end
+	end
 end
