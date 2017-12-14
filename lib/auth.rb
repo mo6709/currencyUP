66ee5bb40698718c106a643febfef16ad8455e4a
Moshe Levi <mo6709@gmail.com>
2017-12-14 18:33:24 -0500

    fix auth.rb to rescue errors

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,11 +1,17 @@
-require 'JWT'
+require 'jwt'
 
 class Auth
-	def self.create_token()
-		
+	def self.create_token(account_object)
+		payload = { account: JSON.parse(account_object.to_json) }
+		JWT.encode(payload, 'myapp', 'HS256')
 	end
 
-	def self.decode_token()
-		
+	def self.decode_token(token)
+        begin
+		    JWT.decode( token, "myapp", true, {  algorithm: 'HS256' })
+		rescue 
+			# Handle invalid token, e.g. logout user or deny access
+			puts 'Audience Error'
+		end
 	end
 end
