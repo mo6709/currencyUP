5a0d42a181fc57e5b1181a60ac713fd6969bf05c
Moshe Levi <mo6709@gmail.com>
2018-01-04 17:53:13 -0500

    edit transactionSerilaizer attributes

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,3 @@
 class TransactionSerializer < ActiveModel::Serializer
-  attributes :id, :investor, :corporation, :currency_id, :t_type
+  attributes :id, :investor, :corporation, :currency_id, :t_type, :total_amount, :return_rate, :dolar_rate
 end
