0031647e03dfa319624228afb2fb4eddf960edba
Moshe Levi <mo6709@gmail.com>
2018-02-07 15:57:07 -0500

    add ENV variable for jwt token

--- app/serializers/corporation_serializer.rb
+++ app/serializers/corporation_serializer.rb
@@ -1,5 +1,5 @@
 class CorporationSerializer < ActiveModel::Serializer
-    attributes :id, :name, :email, :title, :regions_array, :investment_period 
+    attributes :id, :name, :email, :title, :regions_array, :investment_period, :created_at, :updated_at
     #check http://www.rubydoc.info/gems/active_model_serializers/0.8.2/ActiveModel/Serializer
     #for more options
     has_many :currency_corporations
--- app/serializers/investor_serializer.rb
+++ app/serializers/investor_serializer.rb
@@ -1,5 +1,5 @@
 class InvestorSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :region, :email
+    attributes :id, :first_name, :last_name, :region, :email, :created_at, :updated_at
 
     has_many :currency_investors
 	has_many :currencies, through: :currency_investors
