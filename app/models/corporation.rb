5e625453dc9bcf4d0f06dd58abb926b0079fc160
Moshe Levi <mo6709@gmail.com>
2018-02-11 15:04:41 -0500

    add validation to corporation model

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -4,7 +4,7 @@ class Corporation < ApplicationRecord
     
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :investment_period, :in => [2, 4, 6]
+    validates :investment_period, :in => [2, 4, 6, 8]
 
 	has_many :currency_corporations
 	has_many :currencies, through: :currency_corporations
