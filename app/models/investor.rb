3bbf4fc73815444a38bf0defb58a47539bdcc90a
Moshe Levi <mo6709@gmail.com>
2018-02-11 13:36:47 -0500

    add validation of region to investor modle

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -3,6 +3,7 @@ class Investor < ApplicationRecord
 	
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+	validates :region, presence: true
     validates :email, presence: true, uniqueness: true
 
 	has_many :currency_investors
